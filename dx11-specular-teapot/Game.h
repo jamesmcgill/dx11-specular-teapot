c2a41dce43d44ad1e83859b15be5185f713476cb
jamesmcgill <jamesmcgill@users.noreply.github.com>
2017-05-03 17:07:09 +0200

    Add .clang-format and .editorconfig. Run clang-format over all source files.

--- dx11-specular-teapot/Game.h
+++ dx11-specular-teapot/Game.h
@@ -7,48 +7,45 @@
 #include "DeviceResources.h"
 #include "StepTimer.h"
 
-
 // A basic game implementation that creates a D3D11 device and
 // provides a game loop.
 class Game : public DX::IDeviceNotify
 {
 public:
+	Game();
 
-    Game();
+	// Initialization and management
+	void Initialize(HWND window, int width, int height);
 
-    // Initialization and management
-    void Initialize(HWND window, int width, int height);
+	// Basic game loop
+	void Tick();
 
-    // Basic game loop
-    void Tick();
+	// IDeviceNotify
+	virtual void OnDeviceLost() override;
+	virtual void OnDeviceRestored() override;
 
-    // IDeviceNotify
-    virtual void OnDeviceLost() override;
-    virtual void OnDeviceRestored() override;
+	// Messages
+	void OnActivated();
+	void OnDeactivated();
+	void OnSuspending();
+	void OnResuming();
+	void OnWindowSizeChanged(int width, int height);
 
-    // Messages
-    void OnActivated();
-    void OnDeactivated();
-    void OnSuspending();
-    void OnResuming();
-    void OnWindowSizeChanged(int width, int height);
-
-    // Properties
-    void GetDefaultSize( int& width, int& height ) const;
+	// Properties
+	void GetDefaultSize(int& width, int& height) const;
 
 private:
+	void Update(DX::StepTimer const& timer);
+	void Render();
 
-    void Update(DX::StepTimer const& timer);
-    void Render();
+	void Clear();
 
-    void Clear();
+	void CreateDeviceDependentResources();
+	void CreateWindowSizeDependentResources();
 
-    void CreateDeviceDependentResources();
-    void CreateWindowSizeDependentResources();
+	// Device resources.
+	std::unique_ptr<DX::DeviceResources> m_deviceResources;
 
-    // Device resources.
-    std::unique_ptr<DX::DeviceResources>    m_deviceResources;
-
-    // Rendering loop timer.
-    DX::StepTimer                           m_timer;
+	// Rendering loop timer.
+	DX::StepTimer m_timer;
 };
